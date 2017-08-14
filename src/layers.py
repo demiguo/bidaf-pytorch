5797d87816885896e91db790cc37b018370d2324
demi <demiguo98@gmail.com>
2017-08-13 18:16:18 -0700

    Add more layers

--- src/layers.py
+++ src/layers.py
@@ -3,36 +3,95 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 
-class CharEmbedding(nn.Module):
-	def __init__(self, char_single_embedding_dim, char_embedding_dim, filter_height, dropout, char_vocab_size):
-		super(CharEmbedding, self).__init__()
-		self.char_single_embedding_dim = char_single_embedding_dim
-		self.char_embedding_dim = char_embedding_dim
-		self.filter_height = filter_height
-		self.dropout = dropout
-		self.char_vocab_size = char_vocab_size
+class MyLinear(nn.Module):
+    def __init__(self, input_size, output_size, dropout=1.0):
+        super(MyLinear, self).__init__()
+        self.linear = nn.Linear(input_size, output_size)
+        self.dropout = dropout
 
-		self.embedding_lookup = nn.Embedding(self.char_vocab_size, char_single_embedding_dim)
-		self.cnn = nn.Conv1d(char_single_embedding_dim, char_embedding_dim, filter_height, padding=0)
+    def forward(self, x, is_train):
+        if dropout < 1.0:
+            assert is_train is not None
+            if is_train:
+                x = nn.Dropout(p=dropout)(x)
+            x = self.linear(x)
+        return x
 
-	def forward(self, text_char):
-		batch_size, max_length, max_word_length = text_char.size()
 
-		# embedding look up
-		text_char = text_char.contiguous().view(batch_size * max_length, max_word_length)
-		text_char = self.embedding_lookup(text_char)
-		assert text_char.size() == (batch_size * max_length, max_word_length, self.char_single_embedding_dim)
-		
-		# cnn
-		text_char = torch.transpose(text_char, 1, 2)
-		assert text_char.size() == (batch_size * max_length, self.char_single_embedding_dim, max_word_length)
- 		text_char = self.cnn(text_char)
- 		assert text_char.size() == (batch_size * max_length, self.char_embedding_dim, -1)
- 		text_char = text_char.contiguous().view(batch_size * max_length * self.char_embedding_dim, -1)
- 		text_char = nn.functional.relu(text_char)
+class CharEmbeddingLayer(nn.Module):
+    def __init__(self, char_single_embedding_dim, char_embedding_dim, filter_height, dropout, char_vocab_size):
+        super(CharEmbeddingLayer, self).__init__()
+        self.char_single_embedding_dim = char_single_embedding_dim
+        self.char_embedding_dim = char_embedding_dim
+        self.filter_height = filter_height
+        self.dropout = dropout
+        self.char_vocab_size = char_vocab_size
 
- 		# maxpool 
- 		text_char = torch.max(text_char, 1)
- 		assert text_char.size() == (batch_size * max_length * self.char_embedding_dim, 1)
- 		text_char = text_char.contiguous().view(batch_size, max_length, self.char_embedding_dim)
- 		return text_char
+        self.embedding_lookup = nn.Embedding(self.char_vocab_size, char_single_embedding_dim)
+        self.cnn = nn.Conv1d(char_single_embedding_dim, char_embedding_dim, filter_height, padding=0)
+
+    def forward(self, text_char, is_train):
+        batch_size, max_length, max_word_length = text_char.size()
+
+        # embedding look up
+        text_char = text_char.contiguous().view(batch_size * max_length, max_word_length)
+        text_char = self.embedding_lookup(text_char)
+        assert text_char.size() == (batch_size * max_length, max_word_length, self.char_single_embedding_dim)
+        
+        # dropout + cnn
+        if self.dropout < 1.0:
+            assert is_train is not None
+            if is_train:
+                text_char = nn.Dropout(p=self.dropout)(text_char)
+        text_char = torch.transpose(text_char, 1, 2)
+        assert text_char.size() == (batch_size * max_length, self.char_single_embedding_dim, max_word_length)
+        text_char = self.cnn(text_char)
+        assert text_char.size() == (batch_size * max_length, self.char_embedding_dim, -1)
+        text_char = text_char.contiguous().view(batch_size * max_length * self.char_embedding_dim, -1)
+        text_char = nn.functional.relu(text_char)
+
+        # maxpool 
+        text_char = torch.max(text_char, 1)
+        assert text_char.size() == (batch_size * max_length * self.char_embedding_dim, 1)
+        text_char = text_char.contiguous().view(batch_size, max_length, self.char_embedding_dim)
+        return text_char
+
+
+class HighwayNetwork(nn.Module):
+    def __init__(self, size, num_layers, dropout):
+        super(HighwayNetwork, self).__init__()
+
+        self.num_layers = num_layers
+        self.size = size
+
+        self.trans = nn.ModuleList([MyLinear(size, size, dropout) for _ in range(num_layers)])
+        self.gate = nn.ModuleList([MyLinear(size, size, dropout) for _ in range(num_layers)])
+
+    def forward(self, x, is_train):
+        assert x.size() == (-1, size)
+        for layer in range(self.num_layers):
+            gate = nn.functional.sigmoid(self.gate[layer](x, is_train))
+            trans = nn.functional.relu(self.trans[layer](x, is_train))
+            x = gate * trans + (1 - gate) * x
+        return x
+
+# QS(demi): only softmax per sentence not per paragraph???
+
+class AttentionLayer(nn.Module):
+    def __init__(self, config):
+        self.config = config
+
+    def bi_attention(self, is_train, h, u):
+        print "not implemented"
+
+    def forward(self, h, u, is_train):
+        # assume we always use q2c and c2q attention
+        batch_size, max_p_length, contextual_dim = h.size()
+        # by default, contextual_dim = 2d (d is hidden_dim)
+        u_a, h_a = self.bi_attention(is_train, h, u)
+        assert u_a.size() == h_a.size()
+        assert h_a.size() == (batch_size, max_p_length, contextual_dim)
+
+        g = torch.cat((h, u_a, h * u_a, h * h_a), 2)
+        assert g.size() == (batch_size, max_p_length, 4 * contextual_dim)
+        return g
--- src/model.py
+++ src/model.py
@@ -1,11 +1,19 @@
 # todo
-# 3.understand highway network
-# 4.implement highway network
-# 5.understand attention layer
+
+
+# 5.5: reformat data -> change it to per sentence, not per paragraph
+# 5.6: add masks
+
 # 6.implement attention layer
 # 7.understand loss 
 # 8.implement loss layer
 # 9.skip "get_answer"; come back later
+# 10. data processing (to something that i understand
+    # know how to convert to a Dataset)
+# 11.finish generic main framework + config + trainer 
+# 12. finish evaluator + other
+# 13. finish all + debug
+
 import torch
 import torch.nn as nn 
 
@@ -17,136 +25,140 @@ import pandas as pd
 import numpy as np
 
 class BiDAF(nn.module):
-	def __init__(self, config):
-		self.config = config 
-		self.vocab_size = self.config.args.vocab_size
-		self.hidden_dim = self.config.args.hidden_dim
+    def __init__(self, config):
+        self.config = config 
+        self.vocab_size = self.config.args.vocab_size
+        self.hidden_dim = self.config.args.hidden_dim
 
-		# embedding layer
-		self.embedding_dim = self.hidden_dim
-		self.char_embedding_layer = nn.layers.CharEmbedding(\
-			self.config.args.char_single_embedding_dim,
-			self.config.args.char_embedding_dim, # out_channel_dim
-			self.config.args.filter_height,
-			self.config.args.dropout,
-			self.config.args.char_vocab_size)
-		self.word_embedding_layer = nn.Embedding(self.vocab_size, self.config.args.word_embedding_dim)
-		self.init_word_embedding()
-		self.highway_network = ??? # used to combine character and word embedding
+        # embedding layer
+        self.char_embedding_dim = self.config.args.char_embedding_dim
+        self.word_embedding_dim = self.config.args.word_embedding_dim
 
-		# contextual layer
-		self.contextual_dim = self.hidden_dim * 2  
-		self.context_biLSTM = nn.LSTM(self.embedding_dim, self.contextual_dim // 2, num_layers=1, bidirectional=True, dropout=0.5, batch_first=True)
+        # QS(demi): is this correct? in this case, self.embedding_dim may not equal to self.hidden_dim
+        self.embedding_dim = self.char_embedding_dim + self.word_embedding_dim
+        self.char_embedding_layer = nn.layers.CharEmbeddingLayer(\
+            self.config.args.char_single_embedding_dim,
+            self.char_embedding_dim, # out_channel_dim
+            self.config.args.filter_height,
+            self.config.args.dropout,
+            self.config.args.char_vocab_size)
+        self.word_embedding_layer = nn.Embedding(self.vocab_size, self.word_embedding_dim)
+        self.init_word_embedding()
+        self.highway_network = layers.HighwayNetowrk(self.embedding_dim, self.config.args.highway_num_layers, self.config.args.dropout)
 
-		# attention layer
-		self.attention_dim = self.hidden_dim * 8
-		self.attention_layer = ??
+        # contextual layer
+        self.contextual_dim = self.hidden_dim * 2  
+        self.context_biLSTM = nn.LSTM(self.embedding_dim, self.contextual_dim // 2, num_layers=1, bidirectional=True, dropout=0.5, batch_first=True)
 
-		# model layer
-		self.model_dim = self.hidden_dim * 2
-		self.model_biLSTM = nn.LSTM(self.attention_dim, self.model_dim // 2, num_layers=1, bidirectional=True, dropout=0.5, batch_first=True)
+        # attention layer
+        self.attention_dim = self.hidden_dim * 8
+        self.attention_layer = nn.layers.AttentionLayer(self.config)
 
-		# output layer 
-		self.w_p1 = nn.Linear(self.attention_dim + self.model_dim, 1, bias=False)
-		self.w_p2 = nn.Linear(self.attention_dim + self.model_dim, 1, bias=False)
-		self.output_biLSTM = nn.LSTM(self.model_dim, self.model_dim // 2, num_layers=1, bidirectional=True, dropout=0.5, batch_first=True)
+        # model layer
+        self.model_dim = self.hidden_dim * 2
+        self.model_biLSTM = nn.LSTM(self.attention_dim, self.model_dim // 2, num_layers=1, bidirectional=True, dropout=0.5, batch_first=True)
+
+        # output layer 
+        self.w_p1 = nn.Linear(self.attention_dim + self.model_dim, 1, bias=False)
+        self.w_p2 = nn.Linear(self.attention_dim + self.model_dim, 1, bias=False)
+        self.output_biLSTM = nn.LSTM(self.model_dim, self.model_dim // 2, num_layers=1, bidirectional=True, dropout=0.5, batch_first=True)
 
 
-	def init_config(passages, questions, answers):
-		self.batch_size = passages.size(0)
+    def init_config(passages, questions, answers):
+        self.batch_size = passages.size(0)
 
-	def init_hidden(hidden_size, num_layer = 1):
-		if self.config.args.use_cuda:
-			h0 = autograd.Variable(torch.randn(2 * num_layer, self.batch_size, hidden_size // 2).cuda())
-			c0 = autograd.Variable(torch.randn(2 * num_layer, self.batch_size, hidden_size // 2).cuda())
-		else:
-			h0 = autograd.Variable(torch.randn(2 * num_layer, self.batch_size, hidden_size // 2))
-			c0 = autograd.Variable(torch.randn(2 * num_layer, self.batch_size, hidden_size // 2)
-		return (h0, c0)
-
-	def init_word_embedding(self):
-		glove_weight = np.loadtxt(self.config.args.glove_file)
+    def init_hidden(hidden_size, num_layer = 1):
         if self.config.args.use_cuda:
-        	self.word_embedding_layer.weight.data.copy_(torch.FloatTensor(glove_weight).cuda())
+            h0 = autograd.Variable(torch.randn(2 * num_layer, self.batch_size, hidden_size // 2).cuda())
+            c0 = autograd.Variable(torch.randn(2 * num_layer, self.batch_size, hidden_size // 2).cuda())
         else:
-        	self.word_embedding_layer.weight.data.copy_(torch.FloatTensor(glove_weight))
+            h0 = autograd.Variable(torch.randn(2 * num_layer, self.batch_size, hidden_size // 2))
+            c0 = autograd.Variable(torch.randn(2 * num_layer, self.batch_size, hidden_size // 2)
+        return (h0, c0)
+
+    def init_word_embedding(self):
+        glove_weight = np.loadtxt(self.config.args.glove_file)
+        if self.config.args.use_cuda:
+            self.word_embedding_layer.weight.data.copy_(torch.FloatTensor(glove_weight).cuda())
+        else:
+            self.word_embedding_layer.weight.data.copy_(torch.FloatTensor(glove_weight))
         self.word_embedding_layer.weight.requires_grad = False
 
 
-	def loss(p_1, p_2, answers):
-		self.config.error("in model/loss: not implemented")
+    def loss(p_1, p_2, answers):
+        self.config.error("in model/loss: not implemented")
 
-	def get_answer(p_1, p_2, passages):
-		self.config.error("in model/get_answer: not implemented")
+    def get_answer(p_1, p_2, passages):
+        self.config.error("in model/get_answer: not implemented")
 
-	def forward(passages, questions, answers=[]):
-		init_config(passages, questions, answers)
-		# NB(demi): let's first don't consider optimization, and consider (passage, question, answer) triples
-		#            the format of answers are not finalized
-		#            let's first consider all words in passages are concatenated, and all words in a question is concatenated
-		#            let's first ignore masking, but manually padded to same length (max_p_length, max_q_length)
-		# passages: batch_size * max_p_length (in word_indices)
-		# passages_char: batch_size * max_p_length * max_word_size (in character indices)
-		# questions are similar format
+    def forward(passages, questions, answers=[]):
+        init_config(passages, questions, answers)
+        # NB(demi): let's first don't consider optimization, and consider (passage, question, answer) triples
+        #            the format of answers are not finalized
+        #            let's first consider all words in passages are concatenated, and all words in a question is concatenated
+        #            let's first ignore masking, but manually padded to same length (max_p_length, max_q_length)
+        # passages: batch_size * max_p_length (in word_indices)
+        # passages_char: batch_size * max_p_length * max_word_size (in character indices)
+        # questions are similar format
 
-		# QS(demi): is it ok to store all different variables? will it increase the overall memory?
-		### EMBEDDING LAYER ###
+        # QS(demi): is it ok to store all different variables? will it increase the overall memory?
+        ### EMBEDDING LAYER ###
 
-		# QS(demi): is it shared?
-		p_char_embed = self.char_embedding_layer(passages_char)
-		q_char_embed = self.char_embedding_layer(questions_char)
-		assert p_char_embed.size() == (self.batch_size, self.max_p_length, self.char_embedding_dim)
-		assert q_char_embed.size() == (self.batch_size, self.max_q_length, self.char_embedding_dim)
-		
-		p_word_embed = self.word_embedding_layer(passages_char)
-		q_word_embed = self.word_embedding_layer(questions_char)
-		assert p_word_embed.size() == (self.batch_size, self.max_p_length, self.word_embedding_dim)
-		assert q_word_embed.size() == (self.batch_size, self.max_q_length, self.word_embedding_dim)
+        # QS(demi): is it shared? 
+        p_char_embed = self.char_embedding_layer(passages_char, self.training)
+        q_char_embed = self.char_embedding_layer(questions_char, self.training)
+        assert p_char_embed.size() == (self.batch_size, self.max_p_length, self.char_embedding_dim)
+        assert q_char_embed.size() == (self.batch_size, self.max_q_length, self.char_embedding_dim)
+        
+        p_word_embed = self.word_embedding_layer(passages_char)
+        q_word_embed = self.word_embedding_layer(questions_char)
+        assert p_word_embed.size() == (self.batch_size, self.max_p_length, self.word_embedding_dim)
+        assert q_word_embed.size() == (self.batch_size, self.max_q_length, self.word_embedding_dim)
 
-		# combine embedding
-		p_concat_embed = torch.cat((p_char_embed, p_word_embed), 2)
-		q_concat_embed = torch.cat((q_char_embed, q_word_embed), 2)
-		p_embed_X = self.highway_network(p_concat_embed)
-		q_embed_Q = self.highway_netowrk(q_concat_embed)
-		assert p_concat_embed.size() == (self.batch_size, self.max_p_length, self.char_embedding_dim + self.word_embedding_dim)
-		assert q_concat_embed.size() == (self.batch_size, self.max_q_length, self.char_embedding_dim + self.word_embedding_dim)
-		assert p_embed_X.size() == (self.batch_size, self.max_p_length, self.embedding_dim)
-		assert q_embed_Q.size() == (self.batch_size, self.max_q_length, self.embedding_dim)
+        # combine embedding
+        p_concat_embed = torch.cat((p_char_embed, p_word_embed), 2)
+        q_concat_embed = torch.cat((q_char_embed, q_word_embed), 2)
+        p_embed_X = self.highway_network(p_concat_embed, self.training)
+        q_embed_Q = self.highway_netowrk(q_concat_embed, self.training)
+        assert p_concat_embed.size() == (self.batch_size, self.max_p_length, self.char_embedding_dim + self.word_embedding_dim)
+        assert q_concat_embed.size() == (self.batch_size, self.max_q_length, self.char_embedding_dim + self.word_embedding_dim)
+        assert p_embed_X.size() == (self.batch_size, self.max_p_length, self.embedding_dim)
+        assert q_embed_Q.size() == (self.batch_size, self.max_q_length, self.embedding_dim)
 
 
-		### CONTEXTUAL EMBEDDING LAYER ###
-		self.p_context_hidden = self.init_hidden(self.contextual_dim)
-		self.q_context_hidden = self.init_hidden(self.contextual_dim)
+        ### CONTEXTUAL EMBEDDING LAYER ###
+        self.p_context_hidden = self.init_hidden(self.contextual_dim)
+        self.q_context_hidden = self.init_hidden(self.contextual_dim)
 
-		p_context_H, self.p_context_hidden = self.context_biLSTM(p_embed_X, self.p_context_hidden)
-		q_context_U, self.q_context_hidden = self.context_biLSTM(q_embed_Q, self.q_context_hidden)
-		assert p_context_H.size() == (self.batch_size, self.max_p_length, self.contextual_dim)
-		assert q_context_U.size() == (self.batch_size, self.max_q_length, self.contextual_dim)
+        p_context_H, self.p_context_hidden = self.context_biLSTM(p_embed_X, self.p_context_hidden)
+        q_context_U, self.q_context_hidden = self.context_biLSTM(q_embed_Q, self.q_context_hidden)
+        assert p_context_H.size() == (self.batch_size, self.max_p_length, self.contextual_dim)
+        assert q_context_U.size() == (self.batch_size, self.max_q_length, self.contextual_dim)
 
-		### ATTENTION LAYER ###
-		G = self.attention_layer(p_context_H, q_context_U)
-		assert G.size() == (self.batch_size, self.max_p_length, self.attention_dim)
+        ### ATTENTION LAYER ###
+        G = self.attention_layer(p_context_H, q_context_U, self.training)
+        assert G.size() == (self.batch_size, self.max_p_length, self.attention_dim)
 
-		### MODELING LAYER ###
-		self.model_hidden = self.init_hidden(self.model_dim)
-		M, self.model_hidden = self.model_biLSTM(G, self.model_hidden)
-		assert M.size() == (self.batch_size, self.max_p_length, self.model_dim)
-		
+        ### MODELING LAYER ###
+        self.model_hidden = self.init_hidden(self.model_dim)
+        M, self.model_hidden = self.model_biLSTM(G, self.model_hidden)
+        assert M.size() == (self.batch_size, self.max_p_length, self.model_dim)
+        
 
-		### OUTPUT LAYER ###
-		p_1 = torch.cat((G, M), 2)
-		p_1 = self.w_p1(p_1)
-		assert p_1.size() == (self.batch_size, self.max_p_length, 1)
-		p_1 = p_1.contiguous().view(self.batch_size, self.max_p_length)
-		p_1 = nn.Softmax()(p_1)
+        ### OUTPUT LAYER ###
+        p_1 = torch.cat((G, M), 2)
+        p_1 = self.w_p1(p_1)
+        assert p_1.size() == (self.batch_size, self.max_p_length, 1)
+        p_1 = p_1.contiguous().view(self.batch_size, self.max_p_length)
+        p_1 = nn.Softmax()(p_1)
 
-		self.output_hidden = self.init_hidden(self.model_dim)
-		M_2, self.output_hidden = self.output_biLSTM(M, self.output_hidden)
-		p_2 = torch.cat((G, M_2), 2)
-		p_2 = self.w_p2(p_2)
-		p_2 = nn.Softmax()(p_2)
+        self.output_hidden = self.init_hidden(self.model_dim)
+        M_2, self.output_hidden = self.output_biLSTM(M, self.output_hidden)
+        p_2 = torch.cat((G, M_2), 2)
+        p_2 = self.w_p2(p_2)
+        p_2 = nn.Softmax()(p_2)
 
-		if self.training:
-			return loss(p_1, p_2, answers)
-		else:
-			return get_answer(p_1, p_2, passages)
+        if self.training:
+            return loss(p_1, p_2, answers)
+        else:
+            return get_answer(p_1, p_2, passages)
